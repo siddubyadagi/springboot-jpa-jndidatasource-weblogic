7af864f1812a428cc700de57e37fb202e3c23379
Sidagond.Byadagi <Sidagond.X.Byadagi.-ND@disney.com>
2020-06-01 18:38:22 -0400

    initial push

---
+++ src/test/java/com/accurate/solutions/springbootjdbcjndinotomcatproject/SpringbootJdbcJndiNotomcatProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.accurate.solutions.springbootjdbcjndinotomcatproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootJdbcJndiNotomcatProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
